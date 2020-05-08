9caa296e4042efec5cef5fcfd4a52c186d39da5a
Sathish Kumar <30808898+raasalgul@users.noreply.github.com>
2020-05-08 08:08:55 +0530

    Update Constants.java

--- Automated/dao/Constants.java
+++ Automated/dao/Constants.java
@@ -1,1 +1,11 @@
 
+package com.junit.generator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+public static final List<String> collections = Arrays.asList("Map","HashMap","Hashtable");
+public static final List<String> jdbcMockitoList = Arrays.asList("queryForRowSet","queryForObject");
+public static final List<String> jdbcReturnList = Arrays.asList("Integer.class","String.class");
+}
